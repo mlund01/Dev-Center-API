ecd3239408a0dc1a1c08c0e532344f35d0da5eb9
Max Lund <mlund@four51.com>
2015-10-17 13:31:00 -0500

    clean up user/oc-vars endpoints and add user/progress endpoints

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var Underscore = require('underscore');
+var Chance = require('chance');
 
+var chance = new Chance();
 
 
 function verifyKeys(key, errors) {
@@ -9,6 +11,7 @@ function verifyKeys(key, errors) {
 }
 
 router.get('/', function(req, res) {
+  //Get User
   db.collection('users').findOne({Identity: req.User.Identity}, {Identity: 0, _id: 0}, function(err, data) {
     if (err) {
       res.status(500).json({msg: 'Could not process request', error: err});
@@ -19,13 +22,18 @@ router.get('/', function(req, res) {
 });
 
 router.post('/oc-vars', function(req, res) {
+  //Save OC Variable
   if (!req.body) {
     res.status(406).json({error: 'request body is required'})
-  } else if (Underscore.size(req.body) > 1) {
-    res.status(406).json({error: 'cannot add more than one variable at a time'})
-  } else {
+  } else if (!req.body.key) {
+    res.status(406).json({error: "must provided 'key' in request body"});
+  } else if (!req.body.val) {
+    res.status(406).json({error: "must provided 'val' in request body"});
+  }
+  else
+   {
 
-    db.collection('users').updateOne({Identity: req.User.Identity}, {"$push": {"Courses.OcVars": {key: Underscore.keys(req.body)[0], val: Underscore.values(req.body)[0]}}}, function(err, data) {
+    db.collection('users').updateOne({Identity: req.User.Identity}, {"$push": {"Courses.OcVars": {key: req.body.key, val: req.body.val, hash: chance.hash({length: 15})}}}, function(err, data) {
       if (err) {
         res.status(500).json({msg: 'could not add value', error: err});
       } else if (data.result.nModified == 0){
@@ -39,6 +47,7 @@ router.post('/oc-vars', function(req, res) {
 });
 
 router.get('/oc-vars', function(req, res) {
+  //Get OC Variable
   db.collection('users').findOne({Identity: req.User.Identity}, function(err, data) {
     if (err) {
       res.status(500).json({msg: 'Could not get variables', error: err})
@@ -48,41 +57,72 @@ router.get('/oc-vars', function(req, res) {
   })
 });
 
-router.delete('/oc-vars', function(req, res) {
-  if (!req.body) {
-    res.status(406).json({error: 'request body is required'})
-  } else if (Underscore.size(req.body) > 1) {
-    res.status(406).json({error: 'cannot add more than one variable at a time'})
-  } else {
+router.delete('/oc-vars/:hash', function(req, res) {
+  //Delete OC Variable
+  db.collection('users').updateOne({Identity: req.User.Identity}, {"$pull": {"Courses.OcVars": {"hash": req.params.hash}}}, function(err, data) {
+    if (err) {
+      res.status(500).json({msg: 'could not delete', error: err});
+    } else if (data.result.nModified == 0){
+      res.status(404).json({error: 'Could not delete'})
+    } else {
+      res.status(204).send();
+    }
+  })
+});
 
-    db.collection('users').updateOne({Identity: req.User.Identity, "Courses.OcVals.key": Underscore.keys(req.body)[0]}, {"$unset": {"Courses.OcVals.$": ""}}, function(err, data) {
-      if (err) {
-        res.status(500).json({msg: 'could not add value', error: err});
-      } else if (data.result.nModified == 0){
-        res.status(404).json({error: 'Could not update'})
-      } else {
-        res.status(204).send();
-      }
-    })
+router.patch('/oc-vars/:hash', function(req, res) {
+  //Update OC Variable
+  if (!req.body) {
+    res.status(406).json({error: 'request body is required'});
+  } else {
+    var updateVals = {};
+    if (req.body.key && req.body.val) {
+      updateVals = {"Courses.OcVars.$.key": req.body.key, "Courses.OcVars.$.val": req.body.val};
+    } else if (req.body.key && !req.body.val) {
+      updateVals = {"Courses.OcVars.$.key": req.body.key};
+    } else if (!req.body.key && req.body.val) {
+      updateVals = {"Courses.OcVars.$.val": req.body.val};
+    }
+    if (Underscore.isEmpty(updateVals)) {
+      res.status(406).json({error: "Must provided either 'key' or 'val' in request body to update"})
+    } else {
+      db.collection('users').updateOne({Identity: req.User.Identity, "Courses.OcVars.hash": req.params.hash}, {"$set": updateVals}, function(err, data) {
+        if (err) {
+          res.status(406).json({msg: 'Cannot update object at this time', error: err});
+        } else if (data.result.nModified == 0) {
+          res.status(406).json({error: 'Variable could not be found'});
+        } else {
+          res.status(204).send();
+        }
+      })
+    }
+
   }
 });
 
-router.patch('/oc-vars', function(req, res) {
-  if (!req.body) {
-    res.status(406).json({error: 'request body is required'})
-  } else if (Underscore.size(req.body) > 1) {
-    res.status(406).json({error: 'cannot add more than one variable at a time'})
-  } else {
-    db.collection('users').updateOne({Identity: req.User.Identity}, {"$pull": {"Courses.OcVars": req.body}}, function(err, data) {
-      if (err) {
-        res.status(406).json({msg: 'Cannot update object at this time', error: err});
-      } else if (data.result.nModified == 0) {
-        res.status(406).json({error: 'Variable could not be found'});
-      } else {
-        res.status(204).send();
-      }
-    })
-  }
+router.post('/progress/:classid', function(req, res) {
+  //Save Class Progress
+  db.collection('classes').findOne({ID: req.params.classid}, function(err, data) {
+    if (err) {
+      res.status(500).json({msg: 'could not access class in db', error: err})
+    } else if (!data) {
+      res.status(404).json({error: req.params.classid + " class does not exist"});
+    } else {
+      db.collection('users').updateOne({Identity: req.User.Identity}, {"$addToSet": {"Courses.Progress.Classes": req.params.classid}}, function(err, data) {
+        if (err) {
+          res.status(500).json({msg: 'could not add classid to user object', error: err});
+        } else if (data.result.nModified == 0) {
+          res.status(204).send();
+        } else {
+          res.status(204).send();
+        }
+      })
+    }
+  })
+});
+
+router.get('/progress/courses/:courseid', function(req, res) {
+  //Get Course Progress
 });
 
 module.exports = router;
